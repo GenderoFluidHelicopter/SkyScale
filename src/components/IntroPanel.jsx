870b5a2da5bcb13438774c44a10a61006c573463
GenderoFluidHelicopter <a.demin@g.nsu.ru>
2023-12-23 20:53:05 +0700

    prolongong work

--- src/components/IntroPanel.jsx
+++ src/components/IntroPanel.jsx
@@ -1,11 +1,28 @@
+import React from 'react';
+import "./intro.css";
+
 export default function MainPanel() {
     return (
-  
-      <div class="content">
-  
-  <h1>SkyScale</h1>
-  
+      <div className='main'>
+        <div className='header'>
+            <h1>SkyScale</h1>
+            <h2>Upscaling the world</h2>
+            
+            <div className='headerlogo'>              
+
+            </div>
+        </div>
+        <div className='image-container'>
+                </div>
+        <div className='3-steps'>
+            <p>Получай самые качественные фото в 3 шага</p>
+            <ul>
+            <li>1. Используй пробную период</li>
+            <li>2. Оформи подписку</li>
+            <li>3. Пользуся программой и улучшай изображения</li>
+            </ul>
+        </div>
+
       </div>
-  
     )
   }
